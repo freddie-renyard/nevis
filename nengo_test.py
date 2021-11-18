9702b7edb2662a30918af1a0625c992f605fde5c
Freddie Renyard <freddierenyard@hotmail.co.uk>
2021-11-18 21:22:10 +0000

    Made the test file neater.

--- nengo_test.py
+++ nengo_test.py
@@ -1,41 +1,47 @@
 import nengo
+from nengo.network import Network
 from nevis.nevis_networks import NevisEnsembleNetwork
 import numpy as np
 
-
-
+# Define the input function to the neural population
 def input_func(t):
     return np.sin(t * 2*np.pi)
 
-"""
-with nengo.Network() as model:
+# Define, build and run a simple NeVIS model
+def run_simple_fpga_model():
+    with model:
 
-    input_node = nengo.Node(input_func)
+        input_node = nengo.Node(input_func)
 
-    fpga_ens = NevisEnsembleNetwork(
-        n_neurons=50,
-        dimensions=1,
-        fpga_serial_addr="/dev/tty.usbserial-FT4ZS6I31",
-    )
+        fpga_ens = NevisEnsembleNetwork(
+            n_neurons=50,
+            dimensions=1,
+            fpga_serial_addr="/dev/tty.usbserial-FT4ZS6I31",
+        )
 
-    nengo.Connection(input_node, fpga_ens.input)
+        nengo.Connection(input_node, fpga_ens.input)
 
-    # Create an output node to display the output value of the 
-    # FPGA ensemble.
-    output_node = nengo.Node(
-        size_in=1, 
-        size_out=1,
-        label="output_node"
-    )
+        # Create an output node to display the output value of the 
+        # FPGA ensemble.
+        output_node = nengo.Node(
+            size_in=1, 
+            size_out=1,
+            label="output_node"
+        )
 
-    nengo.Connection(fpga_ens.output, output_node)
-"""
+        nengo.Connection(fpga_ens.output, output_node)
+
+# Define, build and run a default Nengo network
+def run_default_model():
+    with model:
+        stim = nengo.Node([0])
+        a = nengo.Ensemble(n_neurons=50, dimensions=1)
+        nengo.Connection(stim, a)
 
 model = nengo.Network()
-with model:
-    stim = nengo.Node([0])
-    a = nengo.Ensemble(n_neurons=50, dimensions=1)
-    nengo.Connection(stim, a)
+
+run_simple_fpga_model()
+
 import nengo_gui
 nengo_gui.GUI(__file__).start()
 
