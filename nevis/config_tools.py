08fafb433fb069c62a3891f827bf9a0fd73a3b96
Freddie Renyard <freddierenyard@hotmail.co.uk>
2021-11-19 14:22:19 +0000

    Added relevant error checking to FPGA config wizard

--- nevis/config_tools.py
+++ nevis/config_tools.py
@@ -9,8 +9,6 @@ class ConfigTools:
             print("[NeVIS]: FPGA configuration file does not exist. Running FPGA configuration wizard...")
             fpga_dict = {}
 
-            # TODO add error checking to these steps.
-
             # Obtain a valid FPGA name
             fpga_name = 0
             while type(fpga_name) != str:
@@ -19,14 +17,22 @@ class ConfigTools:
 
             # Obtain a valid baud rate
             baud_rate = ""
-            while type(baud_rate) != int:
-                baud_rate = int(input("Enter desired serial baud rate: "))
+            while True:
+                baud_rate = input("Enter desired serial baud rate: ")
+                if not baud_rate.isdecimal():
+                    print("Invalid input: the input must be an integer; Try again.")
+                else:
+                    baud_rate = int(baud_rate)
+                    break
             fpga_dict["baud_rate"] = baud_rate
                 
-            # Obtain a /dev/* serial port. TODO check how this works on non-POSIX machines.
-            serial_port = 0
-            while type(serial_port) != str:
+            # Obtain a /dev/* serial port. TODO check how this works on non-POSIX machines
+            while True:
                 serial_port = input("Enter serial port in the form '/dev/*': ")
+                if serial_port[0:5] != "/dev/":
+                    print("Invalid input; Try again.")
+                else:
+                    break
             fpga_dict["serial_addr"] = serial_port
             
             with open("nevis/config/fpga_config.json", "w") as json_file:
