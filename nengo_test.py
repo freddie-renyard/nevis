Fixed log calculation bug

nengo_test.py
@@ -9,7 +9,7 @@ def input_func(t):
     return np.sin(t *0.5* 2*np.pi)
 
 def target_function(x):
-    return x**2, x**3
+    return x, x**2, x**3
 
 # Define, build and run a simple NeVIS model
 def run_simple_fpga_model():
@@ -23,12 +23,11 @@ def run_simple_fpga_model():
         fpga_ens = NevisEnsembleNetwork(
             n_neurons=50,
             dimensions=1,
-            compile_design=False,
+            compile_design=True,
             t_pstc=t_pstc,
             tau_rc=tau_rc,
             function=target_function
         )
-        
         nengo.Connection(input_node, fpga_ens.input)
         #nengo.Connection(fpga_ens.output, fpga_ens.input)
         
@@ -38,7 +37,7 @@ def run_simple_fpga_model():
         )
 
         nengo.Connection(input_node, a)
-        output_node = nengo.Node(size_in=2)
+        output_node = nengo.Node(size_in=3)
         nengo.Connection(a, output_node, synapse=t_pstc, function=target_function)
 
 # Define, build and run a default Nengo network

nevis/memory_compiler.py
@@ -395,20 +395,13 @@ class Compiler:
         of the exponents needed to represent the largest and 
         smallest values.
         """
-        max_val = abs(np.amax(target_list))
-        min_val = abs(np.amin(target_list))
+        max_val = np.amax(np.abs(target_list))
+        min_val = np.amin(np.abs(target_list))
 
-        max_exponent = 0
-        while max_val <= 1:
-            max_val *= 2
-            max_exponent += 1
-
-        min_exponent = 0
-        while min_val <= 1:
-            min_val *= 2
-            min_exponent += 1
-
-        return int((max_exponent + min_exponent)/2)
+        max_exponent = math.ceil(math.log2(max_val))
+        min_exponent = math.ceil(math.log2(min_val))
+        
+        return abs(int((max_exponent + min_exponent)/2))
 
     @classmethod
     def test_int_bin_conversion(cls):
@@ -521,7 +514,6 @@ class Compiler:
         
         return final_ints
 
-#Compiler.test_float_compiler()
 """
 NOTES
 

nevis/neuron_classes.py
@@ -421,17 +421,15 @@ class Synapses_Floating(Synapses):
 
     def __init__(self, n_neurons, pstc_scale, decoders_list, encoders_list, n_activ_extra, radix_w, minimum_val, pre_index, post_start_index, verbose=False):
         
-        # Scale the weights by the post-synaptic scaling constant.
-        decoders_list = decoders_list * pstc_scale
+        decoders_list = decoders_list * 1
         self.output_dims = np.shape(decoders_list)[0]
-        print(self.output_dims)
         
         # Clip small values to reduce dynamic range and hence decrease required exponent bit depth.
         if minimum_val != 0:
             for i, weight_list in enumerate(decoders_list):
                 decoders_list[i] = [Compiler.clip_value(x, minimum_val) for x in weight_list]
 
-        scale_w = Compiler.determine_middle_exp(decoders_list[0])
+        scale_w = Compiler.determine_middle_exp(decoders_list.flatten())
         logger.info("Scale_w for %i: %i", post_start_index, scale_w)
         super().__init__(n_neurons, pstc_scale, decoders_list, encoders_list, n_activ_extra, radix_w, scale_w, verbose=False)
 